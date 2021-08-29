45a59ad0d8c8b05c3ed868a49db1163be0961ce6
digiwhale <digiwhale.humphrey@gmail.com>
2021-08-29 19:22:56 -0400

    got rid of unnecessary declarations

--- main.py
+++ main.py
@@ -13,22 +13,19 @@ GPIO.setup(TXDEN_2, GPIO.OUT)
 GPIO.output(TXDEN_1, GPIO.HIGH)
 GPIO.output(TXDEN_2, GPIO.HIGH)
 ser = serial.Serial("/dev/ttySC0", 115200, timeout=0.01)
-data = ''
 total_distance = 0
 total_time = 0
+time_interval = 0.01
 
 try:
     while(1):
-        data_t = ser.read(1000000).decode("utf-8")
-        data += data_t
-        if(1):
+        data = ser.read(1000000).decode("utf-8")
+        total_time += time_interval
+        if (data):
             speed =  len(data)/2.237
-            time_interval = 0.01
             distance = speed * time_interval
             total_distance += distance
-            total_time += time_interval
             print(speed, "m/s", distance, "m")
-            data = ''
             
 except KeyboardInterrupt:    
     print('  Travelled ', round(total_distance, 4), 'm in ', round(total_time, 4), 's')
