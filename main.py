dd1b30c46d6467ce6f34a53f55e84cdd736df69c
digiwhale <digiwhale.humphrey@gmail.com>
2021-08-29 17:50:32 -0400

    added total distance

--- main.py
+++ main.py
@@ -15,19 +15,20 @@ GPIO.output(TXDEN_1, GPIO.HIGH)
 GPIO.output(TXDEN_2, GPIO.HIGH)
 ser = serial.Serial("/dev/ttySC0", 115200, timeout=0.01)
 data = ''
+total_distance = 0
 
 try:
     while(1):
-        start = time.perf_counter()
         data_t = ser.read(1000000).decode("utf-8")
         data += str(data_t)
-        end = time.perf_counter()
         if(data):
             speed =  round(len(data)/2.237, 2)
-            time = end - start
-            distance = speed * time
+            total_time = 0.01
+            distance = speed * total_time
+            total_distance += distance
             print(speed, "m/s", distance, "m")
             data = ''
             
 except KeyboardInterrupt:    
+    print(total_distance, "m", 'travelled')
     exit()
