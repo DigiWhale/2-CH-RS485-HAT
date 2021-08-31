5e4bc53678398abdb0bb6e21c0d4939d8ad5f1f2
digiwhale <digiwhale.humphrey@gmail.com>
2021-08-31 12:47:12 -0400

    added multiprocessing

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # -*- coding:utf-8 -*-
 import RPi.GPIO as GPIO
 import serial
+from multiprocessing import Process,Queue,Pipe
+from github.BerryIMU.gyro-accel-compass.berryIMU import heading
 
 TXDEN_1 = 27
 TXDEN_2 = 22
@@ -18,6 +20,9 @@ total_time = 0
 time_interval = 0.1
 
 try:
+    parent_conn,child_conn = Pipe()
+    p = Process(target=heading, args=(child_conn,))
+    p.start()
     while(1):
         data = ser.read(1000000)
         total_time += time_interval
@@ -25,7 +30,7 @@ try:
             speed =  (len(data)/10)/2.237
             distance = speed * time_interval
             total_distance += distance
-            print(speed, "m/s", distance, "m", data)
+            print(speed, "m/s", distance, "m", parent_conn.recv())
             
 except KeyboardInterrupt:    
     print('  Travelled', round(total_distance, 4), 'm in', round(total_time, 4), 's')
