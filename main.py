a38698888178ba943461f4346a01e23600f128a2
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-07 14:44:40 -0400

    1

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 # -*- coding:utf-8 -*-
 import RPi.GPIO as GPIO
 import serial
-from multiprocessing import Process,Queue,Pipe
-import sys
-sys.path.append('../BerryIMU/compass')
-from berryIMU import headingfunc
+# from multiprocessing import Process,Queue,Pipe
+# import sys
+# sys.path.append('../BerryIMU/compass')
+# from berryIMU import headingfunc
 
 TXDEN_1 = 27
 TXDEN_2 = 22
@@ -23,17 +23,17 @@ time_interval = 0.1
 
 try:
     while(1):
-        parent_conn,child_conn = Pipe()
-        p = Process(target=headingfunc, args=(child_conn,))
-        p.start()
-        heading = parent_conn.recv()
+        # parent_conn,child_conn = Pipe()
+        # p = Process(target=headingfunc, args=(child_conn,))
+        # p.start()
+        # heading = parent_conn.recv()
         data = ser.read(1000000)
         total_time += time_interval
         if (data):
             speed =  (len(data)/10)/2.237
             distance = speed * time_interval
             total_distance += distance
-            print(speed, "m/s", distance, "m", round(heading, 2), "degrees")
+            print(speed, "m/s", distance, "m")
             
 except KeyboardInterrupt:    
     print('  Travelled', round(total_distance, 4), 'm in', round(total_time, 4), 's')
