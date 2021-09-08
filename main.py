31109daf1353b4cbe6b4b811d8da3232b3e47dcb
digiwhale <digiwhale.humphrey@gmail.com>
2021-09-08 14:01:57 -0400

    1

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ import RPi.GPIO as GPIO
 import serial
 import redis
 import json
-# from multiprocessing import Process,Queue,Pipe
-# import sys
-# sys.path.append('../BerryIMU/compass')
-# from berryIMU import headingfunc
+from datetime import datetime
+
 r = redis.Redis(host="192.168.1.4", port=6379, db=0, password='Redis2019!')
-# r.pubsub(ignore_subscribe_messages=True)
 TXDEN_1 = 27
 TXDEN_2 = 22
 GPIO.setmode(GPIO.BCM)
@@ -26,23 +23,21 @@ time_interval = 0.1
 
 try:
     while(1):
-        # parent_conn,child_conn = Pipe()
-        # p = Process(target=headingfunc, args=(child_conn,))
-        # p.start()
-        # heading = parent_conn.recv()
+        stopped = False
         data = ser.read(1000000)
         total_time += time_interval
         if (data):
+            stopped = False
             speed =  (len(data)/10)/2.237
             distance = speed * time_interval
             total_distance += distance
             print(speed, "m/s", distance, "m")
             r.hmset('doppler', {"speed": speed, "distance": distance, "total_distance": total_distance, "total_time": total_time})
-            # r.publish('doppler', json.dumps({
-            # "speed": speed,
-            # "distance": distance
-            # }))
+        elif (stopped == False):
+            stopped = True
+            print("Stopped")
+            r.hmset('doppler', {"speed": 0, "distance": 0, "total_distance": total_distance, "total_time": total_time})
             
 except KeyboardInterrupt:    
-    print('  Travelled', round(total_distance, 4), 'm in', round(total_time, 4), 's')
+    # print('  Travelled', round(total_distance, 4), 'm in', round(total_time, 4), 's')
     exit()
